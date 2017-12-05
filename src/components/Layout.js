891ce672ab3fe2e56102fc8450718534abd38342
Caique Antonelli <caiqueanm@gmail.com>
2017-12-05 18:59:57 -0200

    css events

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client';
+import { USER_CONNECTED } from '../Events';
+
+const socketUrl = "http://192.168.1.115:3231";
 
 export default class Layout extends Component{
+  constructor(props){
+    super(props);
+
+    this.state  = {
+      socket: null,
+      user: null
+    }
+  }
+
+  componentWillMount(){
+    this.initSocket();
+  }
+
+  initSocket = () => {
+    const socket = io(socketUrl);
+    socket.on('connect', () => {
+      console.log("Connected");
+    })
+    this.setState({socket});
+  }
+
+  /* user property */
+  setUser = (user) => {
+    const { socket } = this.state;
+    socket.emit(USER_CONNECTED);
+    this.setState({user})
+  }
 
   render(){
     const {title} = this.props;
