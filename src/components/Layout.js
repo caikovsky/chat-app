eb9acc4c150a39e12d43f2defa8d3d347371422d
Caique Antonelli <caiqueanm@gmail.com>
2017-12-06 03:00:16 -0200

    add react-icons create chatContainer create sidebar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import io from 'socket.io-client';
 import { USER_CONNECTED, LOGOUT } from '../Events';
 import LoginForm from './LoginForm';
+import ChatContainer from './chats/ChatContainer'
 
 const socketUrl = "http://192.168.1.115:3231";
 
@@ -44,11 +45,16 @@ export default class Layout extends Component{
   }
 
   render(){
-    //const {title} = this.props;
-    const {socket} = this.state;
+    const {title} = this.props;
+    const {socket, user} = this.state;
     return(
       <div className="container">
-        <LoginForm socket={socket} setUser={this.setUser} />
+        {
+          !user ?
+          <LoginForm socket={socket} setUser={this.setUser} />
+          :
+          <ChatContainer socket={socket} user={user} logout={this.logout} />
+        }
       </div>
     );
   }
