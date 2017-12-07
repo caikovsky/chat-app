964b0facfa26aa8f5d217ba93d226d5a7d990e0d
Caique Antonelli <caiqueanm@gmail.com>
2017-12-07 09:20:32 -0200

    created component

--- src/components/messages/MessageInput.js
+++ src/components/messages/MessageInput.js
@@ -2,9 +2,88 @@ import React, { Component } from 'react';
 
 
 export default class MessageInput extends Component {
+ constructor(props){
+   super(props);
+
+   this.state = {
+     message: "",
+     isTyping: false,
+   };
+ }
+
+ componentWillUnmount() {
+   this.stopCheckingTyping();
+ }
+
+ handleSubmit = (e) => {
+   e.preventDefault();
+   this.sendMessage();
+   this.setState({message: ""});
+ }
+
+ sendMessage = () => {
+   this.props.sendMessage(this.state.message);
+ }
+
+ sendTyping = () => {
+   this.lastUpdateTime = Date.now();
+
+   if(!this.state.isTyping){
+      this.setState({isTyping: true});
+      this.props.sendTyping(true);
+      this.startCheckingTyping();
+   }
+ }
+
+ startCheckingTyping = () => {
+   console.log("is typing");
+   this.typingInterval = setInterval(() =>{
+     if((Date.now() - this.lastUpdateTime) > 300){
+       this.setState({isTyping: false});
+       this.stopCheckingTyping();
+     }
+   }, 300);
+ }
+
+ stopCheckingTyping = () => {
+   console.log("stop styping");
+   if(this.typingInterval){
+     clearInterval(this.typingInterval)
+     this.props.sendTyping(false);
+   }
+ }
+
   render() {
-    return (
-        <div>MessageInput</div>
-    );
-  }
+    const {message} = this.state;
+
+  		return (
+  			<div className="message-input">
+  				<form
+  					onSubmit={this.handleSubmit}
+  					className="message-form">
+
+  					<input
+  						id = "message"
+  						ref = {"messageinput"}
+  						type = "text"
+  						className = "form-control"
+  						value = {message}
+  						autoComplete = {'off'}
+  						placeholder = "type something"
+  						onKeyUp = {e => {e.keyCode !== 13 && this.sendTyping()}}
+  						onChange = {
+  							({target}) => {
+  								this.setState({message:target.value})
+  							}
+  						}
+  						/>
+  					<button
+  						disabled = {message.length < 1}
+  						type = "submit"
+  						className = "send"
+  					> Send </button>
+  				</form>
+  			</div>
+  		);
+    }
 }
